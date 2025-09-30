68a88407ccd44de55101f35061022ce6ffbd2e0b
Ravlor <hadzasmpn06@gmail.com>
2025-09-30 08:09:33 +0700

    nechan :3

---
+++ nechan2/js.js
@@ -0,0 +1,11 @@
+function showMessage(){
+    document.getElementById("msg").classList.add("show");
+}
+
+function next(){
+    window.location.href = "sos2.html";
+}
+
+function prev(){
+    window.location.href = "sos1.html";
+}
